38e31dc4ce592e28774fed5d173963c15b08bd19
caofeng <cao.feng@zyxr.com>
2017-04-17 17:36:44 +0800

    完善WKWebView相关功能

--- DemoSet/OC和JS相互调用/JSInterOCViewController.m
+++ DemoSet/OC和JS相互调用/JSInterOCViewController.m
@@ -8,9 +8,10 @@
 
 #import "JSInterOCViewController.h"
 #import <WebKit/WebKit.h>
-@interface JSInterOCViewController ()<WKNavigationDelegate,WKUIDelegate>
+@interface JSInterOCViewController ()<WKNavigationDelegate,WKUIDelegate,WKScriptMessageHandler>
 
 @property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UIProgressView    *progressView;
 
 @end
 
@@ -19,32 +20,269 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //介绍:http://www.jianshu.com/p/99c3af6894f4
+    //介绍:http://www.cnblogs.com/allencelee/p/6709599.html
+    
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
+    NSLog(@"===%@",NSHomeDirectory());
+    
+    self.progressView = [[UIProgressView alloc]init];
+    self.progressView.frame = CGRectMake(0, 64, [UIScreen mainScreen].bounds.size.width, 1);
+    [self.view addSubview:self.progressView];
+    self.progressView.progressTintColor = [UIColor redColor];
+    self.progressView.trackTintColor = [UIColor whiteColor];
+    
     
     WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
+    
+    
     configuration.userContentController = [WKUserContentController new];
+    [configuration.userContentController addScriptMessageHandler:self name:@"AppModel"];
+    
+    configuration.processPool = [[WKProcessPool alloc]init];
     
     WKPreferences *preferences = [WKPreferences new];
     preferences.javaScriptCanOpenWindowsAutomatically = YES;
     preferences.minimumFontSize = 30.0;
+    preferences.javaScriptEnabled = YES;
     configuration.preferences = preferences;
-    self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height) configuration:configuration];
     
-    
+    self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 65, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height-65) configuration:configuration];
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"]]];
-    
+    //如果需要处理web导航操作，比如链接跳转、接收响应、在导航开始、成功、失败等时要做些处理，就可以通过实现相关的代理方法
     self.webView.navigationDelegate = self;
+    
+    //主要处理JS脚本，确认框，警告框等
+    self.webView.UIDelegate = self;
     [self.view addSubview:self.webView];
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:NULL];
+    [self.webView addObserver:self forKeyPath:@"title" options:NSKeyValueObservingOptionNew context:NULL];
+    [self.webView addObserver:self forKeyPath:@"loading" options:NSKeyValueObservingOptionNew context:NULL];
+    
+    //[self performSelector:@selector(setupUI) withObject:nil afterDelay:0.0 inModes:@[NSRunLoopCommonModes]];
+
+}
+
+#pragma mark - WKNavigationDelegate
+
+//请求开始前会调用此方法，类似webView的shouldStartLoadWithRequest
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    
+    self.progressView.hidden = NO;
+
+    NSString *hostname = navigationAction.request.URL.host.lowercaseString;
+    
+    NSLog(@"===hostname:%@",hostname);
+    
+    if (navigationAction.navigationType == WKNavigationTypeLinkActivated && ![hostname containsString:@".baudu.com"]) {
+        
+        decisionHandler(WKNavigationActionPolicyCancel);
+
+    } else {
+        
+        decisionHandler(WKNavigationActionPolicyAllow);
+
+    }
+    
+}
+
+// 在响应完成时，会回调此方法
+// 如果设置为不允许响应，web内容就不会传过来
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+    
+    decisionHandler(WKNavigationResponsePolicyAllow);
+    NSLog(@"%s", __FUNCTION__);
+}
+
+// 开始导航跳转时会回调
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(null_unspecified WKNavigation *)navigation {
+    NSLog(@"%s", __FUNCTION__);
+}
+
+// 接收到重定向时会回调
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(null_unspecified WKNavigation *)navigation {
     
     
     
+    NSLog(@"%s", __FUNCTION__);
+}
+
+// 导航失败时会回调
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error {
     
+    NSLog(@"%s", __FUNCTION__);
+}
+
+// 页面内容到达main frame时回调
+- (void)webView:(WKWebView *)webView didCommitNavigation:(null_unspecified WKNavigation *)navigation {
     
+    NSLog(@"%s", __FUNCTION__);
+}
+
+// 导航完成时，会回调（也就是页面载入完成了）
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    
+    NSLog(@"%s", __FUNCTION__);
+}
+
+// 对于HTTPS的都会触发此代理，如果不要求验证，传默认就行
+// 如果需要证书验证，与使用AFN进行HTTPS证书验证是一样的
+- (void)webView:(WKWebView *)webView didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential *__nullable credential))completionHandler {
+    
+    NSLog(@"%s", __FUNCTION__);
+    completionHandler(NSURLSessionAuthChallengePerformDefaultHandling, nil);
+}
+
+#pragma mark - WKScriptMessageHandler
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
     
     
     
     
 }
 
+#pragma mark - WKUIDelegate
+
+
+// 在JS端调用alert函数时，会触发此代理方法。
+// JS端调用alert时所传的数据可以通过message拿到
+// 在原生得到结果后，需要回调JS，是通过completionHandler回调
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler {
+    NSLog(@"%s", __FUNCTION__);
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"alert" message:@"JS调用alert"                 preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler();
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:NULL];
+    NSLog(@"%@", message);
+}
+
+
+// JS端调用confirm函数时，会触发此方法
+// 通过message可以拿到JS端所传的数据
+// 在iOS端显示原生alert得到YES/NO后
+// 通过completionHandler回调给JS端
+- (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL result))completionHandler {
+    
+    NSLog(@"%s", __FUNCTION__);
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"confirm" message:@"JS调用confirm" preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler(YES);
+    }]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler(NO);
+    }]];
+    [self presentViewController:alert animated:YES completion:NULL];
+    
+    NSLog(@"%@", message);
+}
+
+// JS端调用prompt函数时，会触发此方法
+// 要求输入一段文本
+// 在原生输入得到文本内容后，通过completionHandler回调给JS
+- (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(nullable NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString * __nullable result))completionHandler {
+    NSLog(@"%s", __FUNCTION__);
+    
+    NSLog(@"%@", prompt);
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"textinput" message:@"JS调用输入框" preferredStyle:UIAlertControllerStyleAlert];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.textColor = [UIColor redColor];
+    }];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler([[alert.textFields lastObject] text]);
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:NULL];
+}
+
+
+#pragma mark - KVO
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    
+    if ([keyPath isEqualToString:@"title"]) {
+        
+        self.navigationItem.title = self.webView.title;
+        
+    } else if ([keyPath isEqualToString:@"estimatedProgress"]) {
+        
+        [self.progressView setProgress:self.webView.estimatedProgress animated:YES];
+        
+    } else if ([keyPath isEqualToString:@"loading"]) {
+        
+        NSString *js = @"callJsAlert()";
+        [self.webView evaluateJavaScript:js completionHandler:^(id _Nullable response, NSError * _Nullable error) {
+            NSLog(@"response: %@ error: %@", response, error);
+            NSLog(@"call js alert by native");
+        }];
+        
+    }
+    
+    
+    if (!self.webView.loading) {
+        NSLog(@"加载完成");
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            
+            self.progressView.alpha = 0;
+            
+        });
+        
+    }
+}
+
+- (void)dealloc
+{
+    
+    [self.webView removeObserver:self forKeyPath:@"title"];
+    [self.webView removeObserver:self forKeyPath:@"loading"];
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+    [self clearCache];
+    
+}
+
+/** 清理缓存的方法，这个方法会清除缓存类型为HTML类型的文件*/
+- (void)clearCache {
+    /* 取得Library文件夹的位置*/
+    NSString *libraryDir = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory,NSUserDomainMask, YES)[0];
+    /* 取得bundle id，用作文件拼接用*/
+    NSString *bundleId  =  [[[NSBundle mainBundle] infoDictionary]objectForKey:@"CFBundleIdentifier"];
+    /*
+     * 拼接缓存地址，具体目录为App/Library/Caches/你的APPBundleID/fsCachedData
+     */
+    NSString *webKitFolderInCachesfs = [NSString stringWithFormat:@"%@/Caches/%@/fsCachedData",libraryDir,bundleId];
+    
+    NSError *error;
+    /* 取得目录下所有的文件，取得文件数组*/
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    //    NSArray *fileList = [[NSArray alloc] init];
+    //fileList便是包含有该文件夹下所有文件的文件名及文件夹名的数组
+    NSArray *fileList = [fileManager contentsOfDirectoryAtPath:webKitFolderInCachesfs error:&error];
+    /* 遍历文件组成的数组*/
+    for(NSString * fileName in fileList){
+        /* 定位每个文件的位置*/
+        NSString * path = [[NSBundle bundleWithPath:webKitFolderInCachesfs] pathForResource:fileName ofType:@""];
+        /* 将文件转换为NSData类型的数据*/
+        NSData * fileData = [NSData dataWithContentsOfFile:path];
+        /* 如果FileData的长度大于2，说明FileData不为空*/
+        if(fileData.length >2){
+            /* 创建两个用于显示文件类型的变量*/
+            int char1 =0;
+            int char2 =0;
+            
+            [fileData getBytes:&char1 range:NSMakeRange(0,1)];
+            [fileData getBytes:&char2 range:NSMakeRange(1,1)];
+            /* 拼接两个变量*/
+            NSString *numStr = [NSString stringWithFormat:@"%i%i",char1,char2];
+            /* 如果该文件前四个字符是6033，说明是Html文件，删除掉本地的缓存*/
+            if([numStr isEqualToString:@"6033"]){
+                [[NSFileManager defaultManager] removeItemAtPath:[NSString stringWithFormat:@"%@/%@",webKitFolderInCachesfs,fileName]error:&error];
+                continue;
+            }
+        }
+    }
+}
 
 @end
